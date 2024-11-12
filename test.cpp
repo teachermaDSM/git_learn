18671bc837b704480c3e258c16acf9f9bb35d12f
zzs <1415877584@qq.com>
2024-11-12 08:56:12 +0800

    test4

--- test.cpp
+++ test.cpp
@@ -4,5 +4,10 @@ int main()
 {
     cd 111;
     fdasfadsfad;
-    return 0;
+    return 0;dsfasfdsaff
+    _INLINE_VARIABLES_SUPPORTEDfds
+    alignofasdf
+    asdf
+    transaction_safe_dynamicfsd
+    
 }
