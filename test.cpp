d2dd08005d052999a44247f78304fd218281ab1b
zzs <1415877584@qq.com>
2024-11-13 08:16:18 +0800

    test4

--- test.cpp
+++ test.cpp
@@ -3,11 +3,7 @@
 int main()
 {
     cd 111;
-    fdasfadsfad;
-    return 0;dsfasfdsaff
-    _INLINE_VARIABLES_SUPPORTEDfds
-    alignofasdf
-    asdf
-    transaction_safe_dynamicfsd
+    Git commit 111;
+    
     
 }
